21b3d4867e7ea7cedf8ceae9c781efeba110d153
Brendan Asselstine <mail.asselstine@gmail.com>
2016-01-27 21:03:28 -0800

    Added another spec to the SnsController for transcoder notifications

--- app/controllers/sns_controller.rb
+++ app/controllers/sns_controller.rb
@@ -5,13 +5,14 @@ class SnsController < ApplicationController
   def message 
     body = request.body.read
     if Aws::SNS::MessageVerifier.new.authentic?(body)
-      msg = JSON.parse(body)
-      logger.debug("SNS: Received message: #{msg}")
-      if msg['Token']
+      json = JSON.parse(body)
+      logger.debug("SNS: Received message: #{json}")
+      if json['Token']
         topic = ::Aws::SNS::Topic.new(Figaro.env.aws_et_sns_topic_arn)
-        response = topic.confirm_subscription(token: msg['Token'])
+        response = topic.confirm_subscription(token: json['Token'])
         logger.debug("SNS: Confirmed subscription for topic: #{Figaro.env.aws_et_sns_topic_arn}: #{response}")
       else
+        msg = JSON.parse(json['Message'])
         logger.debug("SNS: Message is for job: #{msg['jobId']} with state #{msg['state']}")
         video = Video.where(job_id: msg['jobId']).first
         if video

15e99764f788422a433353f2c228a55e83bd5cdd
Brendan Asselstine <mail.asselstine@gmail.com>
2016-02-04 23:24:29 -0800

    Loosened up the caching window for the UserEvaluator

--- app/evaluators/user_evaluator.rb
+++ app/evaluators/user_evaluator.rb
@@ -1,11 +1,14 @@
 class UserEvaluator < Evaluator
   attribute :user, User
 
-  def coords 
-    time = context.cut_start_at
+  def coords_at(time)
     return nil unless Location.can_interpolate?(time, user_locations)
-    time_ms = Location.to_time_ms(time) 
+    time_ms = Location.to_time_ms(time)
     [lat_spline.eval(time_ms), long_spline.eval(time_ms)]
+  end
+
+  def coords
+    coords_at(context.cut_start_at)
   end
 
   protected
@@ -19,6 +22,6 @@ class UserEvaluator < Evaluator
   end
 
   def user_locations
-    @locations ||= user.locations.order(timestamp: :asc).during(context.start_at, context.end_at)
+    @locations ||= user.locations.order(timestamp: :asc).during(context.start_at - 10.minutes, context.end_at + 10.minutes)
   end
 end

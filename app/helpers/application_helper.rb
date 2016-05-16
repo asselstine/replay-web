3fb7ffdfe0482c1ec5be9d0b46d99095b108e7c2
Brendan Asselstine <mail.asselstine@gmail.com>
2016-05-16 10:46:20 -0700

    Added DirectUploadForm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,18 @@ module ApplicationHelper
       'info'
     end
   end
+
+  # JavaScript: direct_upload_config: JSON.parse('#{raw direct_upload_config.to_json}'),
+  def direct_upload_config(options = {})
+    config_options = {
+      acl: 'public-read',
+      key: "direct_uploads/{timestamp}-{unique_id}-#{SecureRandom.hex}/${filename}",
+      key_starts_with: 'direct_uploads/'
+    }.merge options
+    uploader = S3DirectUpload::UploadHelper::S3Uploader.new(config_options)
+    {
+      fields: uploader.fields,
+      url: uploader.url
+    }
+  end
 end

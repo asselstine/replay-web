cd939ee6e1ea2fd6494df067894e8c46bca9796f
Brendan Asselstine <mail.asselstine@gmail.com>
2016-09-30 15:14:51 -0700

    Removed streamio-ffmpeg from gemfile

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,30 +1,28 @@
 # encoding: utf-8
 
 class VideoUploader < CarrierWave::Uploader::Base
-  process :video_metadata
+  # process :video_metadata
 
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/AbcSize
-  def video_metadata
-    cache_stored_file! unless cached?
-    movie = FFMPEG::Movie.new(current_path)
-    model.update(
-      filename: File.basename(current_path),
-      duration: movie.duration,
-      duration_ms: (movie.duration.to_f * 1000),
-      bitrate: movie.bitrate,
-      size: movie.size,
-      video_stream: movie.video_stream,
-      video_codec: movie.video_codec,
-      colorspace: movie.colorspace,
-      resolution: movie.resolution,
-      width: movie.width,
-      height: movie.height,
-      frame_rate: movie.frame_rate,
-      audio_stream: movie.audio_stream,
-      audio_codec: movie.audio_codec,
-      audio_sample_rate: movie.audio_sample_rate,
-      audio_channels: movie.audio_channels
-    )
-  end
+  # def video_metadata
+  #   cache_stored_file! unless cached?
+  #   movie = FFMPEG::Movie.new(current_path)
+  #   model.update(
+  #     filename: File.basename(current_path),
+  #     duration: movie.duration,
+  #     duration_ms: (movie.duration.to_f * 1000),
+  #     bitrate: movie.bitrate,
+  #     size: movie.size,
+  #     video_stream: movie.video_stream,
+  #     video_codec: movie.video_codec,
+  #     colorspace: movie.colorspace,
+  #     resolution: movie.resolution,
+  #     width: movie.width,
+  #     height: movie.height,
+  #     frame_rate: movie.frame_rate,
+  #     audio_stream: movie.audio_stream,
+  #     audio_codec: movie.audio_codec,
+  #     audio_sample_rate: movie.audio_sample_rate,
+  #     audio_channels: movie.audio_channels
+  #   )
+  # end
 end

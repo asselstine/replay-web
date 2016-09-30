bab5166c0f11f244e1ff4f6b7c2a88352d6ea791
Brendan Asselstine <mail.asselstine@gmail.com>
2016-09-30 14:49:07 -0700

    One more tweak to fix cukes

--- spec/support/strava_null_client.rb
+++ spec/support/strava_null_client.rb
@@ -1,34 +1,30 @@
 require 'json'
 
 class StravaNullClient
-  LIST_ATHLETE_ACTIVITIES = JSON.parse(
-    File.read(
-      Rails.root.join('spec/fixtures/strava_list_athlete_activities.json')
-    )
-  )
-  RETRIEVE_ACTIVITY_STREAMS = JSON.parse(
-    File.read(
-      Rails.root.join('spec/fixtures/strava_retrieve_activity_streams.json')
-    )
-  )
-  RETRIEVE_AN_ACTIVITY = JSON.parse(
-    File.read(
-      Rails.root.join('spec/fixtures/strava_retrieve_an_activity.json')
-    )
-  )
-
   def initialize(access_token)
   end
 
   def list_athlete_activities(*_)
-    LIST_ATHLETE_ACTIVITIES
+    @list_athlete_activities ||= JSON.parse(
+      File.read(
+        Rails.root.join('spec/fixtures/strava_list_athlete_activities.json')
+      )
+    )
   end
 
   def retrieve_activity_streams(*_)
-    RETRIEVE_ACTIVITY_STREAMS
+    @retrieve_activity_streams ||= JSON.parse(
+      File.read(
+        Rails.root.join('spec/fixtures/strava_retrieve_activity_streams.json')
+      )
+    )
   end
 
   def retrieve_an_activity(*_)
-    RETRIEVE_AN_ACTIVITY
+    @retrieve_an_activity ||= JSON.parse(
+      File.read(
+        Rails.root.join('spec/fixtures/strava_retrieve_an_activity.json')
+      )
+    )
   end
 end

1e571238b997308c47014460d31dabd7e742965a
Brendan Asselstine <mail.asselstine@gmail.com>
2016-10-03 23:45:12 -0700

    Maybe fixed bug

--- app/services/hls/streams/create.rb
+++ app/services/hls/streams/create.rb
@@ -7,11 +7,11 @@ module Streams
 
     def call
       et_outputs.each do |output|
-        base = filename_with_prefix(output[:key])
+        base = playlist.job.filename_with_prefix(output[:key])
         attrs = { ts_key: base + '.ts',
                   playlist_key: base + '_v4.m3u8' }
         attrs[:iframe_key] = base + '_iframe.m3u8' if output[:rotate].present?
-        job.playlist.streams.create!(attrs)
+        playlist.streams.create!(attrs)
       end
     end
 
